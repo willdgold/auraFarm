d8a80d40c59d94005857deaaec7c0e9e8fce0c98
William Goldman <willdgold@gmail.com>
2025-06-16 17:39:56 +0200

    think

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>aesthetic.exe</h1>
+        <h1>Aura Farming Manual</h1>
       </header>
       
       <main className="App-main">
